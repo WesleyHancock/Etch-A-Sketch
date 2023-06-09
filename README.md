90b5bc7f10121a572c295fb17f10ffa9decc75b9
WesleyHancock <wesleyhancock@protonmail.com>
2023-06-09 18:49:14 +0200

    05 - added Slider Having trouble fitting squares within the canvas not dynamically changing from large canvas back to a small one

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Etch-A-Sketch
 Something between a sketchpad and an Etch-A-Sketch.
+
+# Provide a selection for various canvas sizes
+16 x 16 has individual width and height of 30px
+The container has height and width of 480px.
+
+1. Need a menu for the user to select options from, such as:
+- canvas size and square ratio
+- color to draw in
+- rainbow color mode
+- eraser
+- reset canvas
+
+# Canvas size and square ratio
+- minumum up to maximum size
+- minumum size 16 x 16 = 256 squares
+- maximum size 64 x 64 = 4096 squares
+- Use an input type range to select the different sizes
+
+- need to retrieve the input
+- multiply the input by itself to get the amount of squares necessary
--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,96 @@
 let container = document.querySelector('.container');
 
-// CREATE 16 X 16 DIVS OR 256 DIVS
-for(let i = 0; i < 256; i++) {
-    let divs = document.createElement('div');
-    divs.classList.add('squares');
-    container.appendChild(divs);
-};
+// CHOICE SLIDER
+let sliderValue = document.querySelector('#sliderValue');
+let slider = document.querySelector('#slider');
+let input = null;
 
 // CREATE A GRID USING FLEXBOX
-container.style.display = 'flex';
-container.style.flexDirection = 'row';
-container.style.flexWrap = 'wrap';
-container.style.maxWidth = '480px';
-container.style.maxHeight = '480px';
+let containerWidth = container.style.maxWidth = '480px';
+let containerHeight = container.style.maxHeight = '480px';
 
-
-let squares = document.querySelectorAll('.squares');
-
-squares.forEach(function(square) {
-    square.style.outline = '1px solid black';
-    square.style.width = '30px';
-    square.style.height = '30px';
-    square.addEventListener('mouseover', function() {
-        // console.log('entered');
-        this.classList.add('black');
-    });
-    // square.addEventListener('mouseout', function() {
-    //     console.log('left');
-    //     // this.classList.toggle('black');
-    // });
+// Display the value to the slider
+slider.addEventListener('input', function() {
+    sliderValue.innerText = `${this.value} X ${this.value}`;
+    grid(slider.value);
 });
+
+function grid(a) {
+    let totSquares = parseInt(a * a);
+
+    for(let i = 0; i < totSquares; i++) {
+        let divs = document.createElement('div');
+        divs.classList.add('squares');
+        container.appendChild(divs);
+        let squareWidth = parseInt(containerWidth) / totSquares;
+        let squareHeight = parseInt(containerHeight) / totSquares;
+    };
+
+    let squares = document.querySelectorAll('.squares');
+
+
+    squares.forEach(function(square) {
+        square.style.outline = '0.1px solid black';
+
+        square.style.width = squareWidth + 'px';
+        square.style.height = squareHeight + 'px';
+        // square.addEventListener('mouseover', function() {
+        //     // console.log('entered');
+        //     this.classList.add('black');
+        // });
+        // square.addEventListener('mouseout', function() {
+        //     console.log('left');
+        //     // this.classList.toggle('black');
+        // });
+    });
+
+};
+
+// let sliderInput = function() {
+//     // percentage formula to make the min value 0 and the max 100
+//     // let value = (this.value - this.min) / (this.max-this.min) * 100;
+//     // 16 = 0
+//     // 64 = 100
+//     sliderValue.innerText = this.value;
+//     return this.value;
+// }
+
+let log = function(a) {
+    // console.log(parseInt(a));
+    let num = parseInt(a);
+    let totalDivs = a * a;
+    // for(let i = 256; i < totalDivs; i++) {
+    //     let divs = document.createElement('div');
+    //     divs.classList.add('squares');
+    //     container.appendChild(divs);
+    // };
+};
+
+
+// need sliderInput * sliderInput
+
+
+// CREATE 16 X 16 DIVS OR 256 DIVS
+// for(let i = 0; i < 256; i++) {
+//     let divs = document.createElement('div');
+//     divs.classList.add('squares');
+//     container.appendChild(divs);
+// };
+
+
+// let squares = document.querySelectorAll('.squares');
+
+// squares.forEach(function(square) {
+//     square.style.outline = '1px solid black';
+//     square.style.width = '30px';
+//     square.style.height = '30px';
+//     // square.addEventListener('mouseover', function() {
+//     //     // console.log('entered');
+//     //     this.classList.add('black');
+//     // });
+//     // square.addEventListener('mouseout', function() {
+//     //     console.log('left');
+//     //     // this.classList.toggle('black');
+//     // });
+// });
+
