12682f764893fd6aafd8d42feab2a7edc3ebe97c
WesleyHancock <wesleyhancock@protonmail.com>
2023-06-12 14:48:17 +0200

    06 - Sidebar options for size and color Added fully funcitoning range slider Added color picker for custom painting in color Added a clear button to reset the chosen grid

--- scripts/script.js
+++ scripts/script.js
@@ -8,46 +8,60 @@ let sliderValue = document.querySelector('#sliderValue');
 
 let slider = document.querySelector('#slider');
 let value = null;
+let color = null;
 
-function retrieveSliderValue(x) {
+let clearBtn = document.querySelector('.clear');
+
+// color picker
+let colorPicker = document.querySelector('#colorPicker');
+colorPicker.addEventListener('input', function(e) {
+    color = e.target.value;
+});
+
+function makeGrid(x) {
 
     // first need to clear old drawings
     // document.querySelectorAll('div .square').remove();
     canvas.innerText = '';
 
-
     // Create amount of divs
-    for(let i = 0; i < x * x; i++) {
+    for(let i = 0; i < (x * x); i++) {
         let divs = document.createElement('div');
-        divs.classList.add('squares');
         divs.style.width = parseInt(canvasWidth) / x + 'px';
         divs.style.height = parseInt(canvasHeight) / x + 'px';
-        divs.style.outline = '0.1px solid black';
+        divs.style.backgroundColor = 'rgb(244, 244, 244)';
+        divs.style.outline = '0.1px solid rgb(196, 196, 196)';
+        divs.addEventListener('dragover', squareColor);
+        divs.classList.add('square');
         canvas.appendChild(divs);
     }
 
-    let squares = document.querySelectorAll('.squares');
-    squares.forEach(function(square) {
-        square.addEventListener('dragover', function() {
-            // console.log('entered');
-            this.classList.add('black');
-        });
-        // square.addEventListener('mouseout', function() {
-        //     console.log('left');
-        //     // this.classList.toggle('black');
-        // });
-    });
+    function squareColor(event) {
+        // console.log('entered');
+        if(color !== null) {
+            this.style.backgroundColor = `${color}`;
+        } else {
+            this.style.backgroundColor = `rgb(0, 0, 0)`;
+        };
+    };
 
     return;
 
-}
+};
+
+function clear() {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(function(square) {
+        square.style.backgroundColor = 'rgb(244, 244, 244)';
+    });
+};
 
 function displaySlider() {
     sliderValue.innerText = `${slider.value} X ${slider.value}`;
     value = slider.value;
-    retrieveSliderValue(value);
-}
+    makeGrid(value);
+};
 
 // Display slider values on the slider
 slider.addEventListener('change', displaySlider);
-
+clearBtn.addEventListener('click', clear);
